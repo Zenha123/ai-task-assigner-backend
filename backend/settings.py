04ced67c8ece8ec34a5fc3fe46a617693ed3c262
Zenha123 <fathimazenha21@gmail.com>
2025-11-06 19:34:20 +0530

    dokcer updated

--- backend/settings.py
+++ backend/settings.py
@@ -116,6 +116,9 @@ DATABASES = {
         "PASSWORD": config("POSTGRES_PASSWORD"),
         "HOST": config("POSTGRES_HOST"),
         "PORT": config("POSTGRES_PORT"),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
@@ -177,8 +180,8 @@ SPECTACULAR_SETTINGS = {
 
 
 # Celery Configuration
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/1"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/1"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
